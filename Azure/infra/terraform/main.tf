6e67be72825dce5514114fd4e986d0ba3f628b62
CPMahan <80348747+CPMahan@users.noreply.github.com>
2023-04-06 10:59:58 -0400

    added ws sa

--- Azure/infra/terraform/main.tf
+++ Azure/infra/terraform/main.tf
@@ -167,7 +167,9 @@ module "log_analytics_workspace" {
   location = var.location
   law_sku = var.law_sku
   data_source_type = var.law_data_source_type
-  storage_account_ids = ["${module.hub_storage_account.id}"]
+  law_storage_account_name = var.hub_storage_account_name
+  account_tier = var.hub_storage_account_tier
+  account_replication_type = var.hub_storage_account_replication_type
   #workspace_resource_id
   #storage_account_ids
   #depends_on
--- Azure/infra/terraform/modules/log_analytics/main.tf
+++ Azure/infra/terraform/modules/log_analytics/main.tf
@@ -5,19 +5,19 @@ resource "azurerm_log_analytics_workspace" "log_analytics_workspace" {
   sku                 = var.law_sku
 }
 
-# module "storage_account" {
-#   source                   = "../storage_account"
-#   storage_account_name     = var.law_storage_account_name
-#   resource_group_name      = var.resource_group_name
-#   location                 = var.location
-#   storage_account_tier     = var.account_tier
-#   account_replication_type = var.account_replication_type
-# }
+module "storage_account" {
+  source                   = "../storage_account"
+  storage_account_name     = var.law_storage_account_name
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  storage_account_tier     = var.account_tier
+  account_replication_type = var.account_replication_type
+}
 
 resource "azurerm_log_analytics_linked_storage_account" "log_analytics_linked_storage_account" {
   data_source_type      = var.data_source_type
   resource_group_name   = var.resource_group_name
   workspace_resource_id = azurerm_log_analytics_workspace.log_analytics_workspace.id
-  storage_account_ids   = var.storage_account_ids
-  depends_on            = [azurerm_log_analytics_workspace.log_analytics_workspace]
+  storage_account_ids   = [module.storage_account.id]
+  depends_on            = [azurerm_log_analytics_workspace.log_analytics_workspace, module.storage_account]
 }
