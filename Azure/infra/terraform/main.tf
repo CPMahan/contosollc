6a51b03a5fc21b9a172645d424cf24968b4d5a8f
CPMahan <80348747+CPMahan@users.noreply.github.com>
2023-04-05 21:59:56 -0400

    adding more depends_on

--- Azure/infra/terraform/main.tf
+++ Azure/infra/terraform/main.tf
@@ -48,6 +48,9 @@ module "bastion_subnet" {
   resource_group_name = var.hub_resource_group_name
   virtual_network_name = var.hub_virtual_network_name
   subnet_address_prefixes = var.bastion_subnet_prefixes
+  depends_on = [
+    module.hub_virtual_network
+  ]
 }
 
 module "app_gateway_subnet" {
@@ -56,6 +59,9 @@ module "app_gateway_subnet" {
   resource_group_name = var.hub_resource_group_name
   virtual_network_name = var.hub_virtual_network_name
   subnet_address_prefixes = var.app_gateway_subnet_prefixes
+  depends_on = [
+    module.hub_virtual_network
+  ]
 }
 
 module "management_tools_subnet" {
@@ -64,6 +70,9 @@ module "management_tools_subnet" {
   resource_group_name = var.hub_resource_group_name
   virtual_network_name = var.hub_virtual_network_name
   subnet_address_prefixes = var.management_tools_subnet_prefixes
+  depends_on = [
+    module.hub_virtual_network
+  ]
 }
 
 module "hub_endpoint_subnet" {
@@ -72,6 +81,9 @@ module "hub_endpoint_subnet" {
   resource_group_name = var.hub_resource_group_name
   virtual_network_name = var.hub_virtual_network_name
   subnet_address_prefixes = var.hub_endpoint_subnet_prefixes
+  depends_on = [
+    module.hub_virtual_network
+  ]
 }
 
 module "dev_kv_subnet" {
@@ -80,6 +92,9 @@ module "dev_kv_subnet" {
   resource_group_name = var.dev_resource_group_name
   virtual_network_name = var.dev_virtual_network_name
   subnet_address_prefixes = var.dev_kv_subnet_prefixes
+  depends_on = [
+    module.dev_virtual_network
+  ]
 }
 
 module "dev_data_subnet" {
@@ -88,6 +103,9 @@ module "dev_data_subnet" {
   resource_group_name = var.dev_resource_group_name
   virtual_network_name = var.dev_virtual_network_name
   subnet_address_prefixes = var.dev_data_subnet_prefixes
+  depends_on = [
+    module.dev_virtual_network
+  ]
 }
 
 module "dev_asp_subnet" {
@@ -96,6 +114,9 @@ module "dev_asp_subnet" {
   resource_group_name = var.dev_resource_group_name
   virtual_network_name = var.dev_virtual_network_name
   subnet_address_prefixes = var.dev_asp_subnet_prefixes
+  depends_on = [
+    module.dev_virtual_network
+  ]
 }
 
 module "dev_asp_endpoint_subnet" {
