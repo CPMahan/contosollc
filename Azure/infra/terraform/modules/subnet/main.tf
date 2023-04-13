f842c5ee5eeb12274c96332d1c39ee2761f93eab
CPMahan <80348747+CPMahan@users.noreply.github.com>
2023-04-13 12:49:00 -0400

    added private_endpoint_network_policies_enabled

--- Azure/infra/terraform/modules/subnet/main.tf
+++ Azure/infra/terraform/modules/subnet/main.tf
@@ -3,4 +3,6 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = var.resource_group_name
   virtual_network_name = var.virtual_network_name
   address_prefixes     = var.subnet_address_prefixes #["10.0.1.0/24"]
+
+  private_endpoint_network_policies_enabled = false
 }
