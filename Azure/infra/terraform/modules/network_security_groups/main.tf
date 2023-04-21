e61b6adda3de24b60df0b336e2aecfd6b7a490ea
CPMahan <80348747+CPMahan@users.noreply.github.com>
2023-04-21 11:20:41 -0400

    changed name for nsg

--- Azure/infra/terraform/modules/network_security_groups/main.tf
+++ Azure/infra/terraform/modules/network_security_groups/main.tf
@@ -4,7 +4,7 @@ resource "azurerm_network_security_group" "nsg" {
   resource_group_name = var.resource_group_name
 
   security_rule {
-    name                       = "basic nsg"
+    name                       = "basic"
     priority                   = 100
     direction                  = "Inbound"
     access                     = "Allow"
